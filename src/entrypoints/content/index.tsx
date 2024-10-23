666e7bc6290daeb7e03c8ee7908e7ea22d19324a
Rohit Sahu <irohitsahu97@gmail.com>
2024-10-23 19:59:28 +0530

    add logic for button inject and focus

--- src/entrypoints/content/index.tsx
+++ src/entrypoints/content/index.tsx
@@ -1,20 +1,82 @@
+// 1. inject the button ui in message box when focused
+// 2. remove the ui form box if focus looses
+// 3.
+
 import "@/styles/global.scss";
 import { createRoot, Root } from "react-dom/client";
 import type { ContentScriptContext } from "wxt/client";
 import App from "@/entrypoints/content/App";
 
+type UiObject = {
+  mount: () => void;
+  remove: () => void;
+};
+
+let ui: UiObject | null = null;
+let activeMessageBox: Element | null = null;
+
 export default defineContentScript({
-  matches: ["*://*/*"],
+  matches: ["https://www.linkedin.com/*"],
   cssInjectionMode: "ui",
 
   async main(ctx) {
-    const ui = await createUi(ctx);
-    ui.mount();
+    // initial check
+    setupMessageBoxListener(ctx);
+
+    // watching for dynamic changes
+    const observer = new MutationObserver(() => {
+      setupMessageBoxListener(ctx);
+    });
+
+    // observing the document body for changes
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true,
+    });
   },
 });
 
-function createUi(ctx: ContentScriptContext) {
-  const messageBox = document.querySelector("#username");
+function setupMessageBoxListener(ctx: ContentScriptContext) {
+  const messageBoxes = document.querySelectorAll(
+    "div.msg-form__contenteditable[contenteditable='true']"
+  );
+
+  // event listeners for each message box
+  messageBoxes.forEach((messageBox) => {
+    // avoiding multiple listeners to the same box
+    if (!messageBox.hasAttribute("data-listener-attached")) {
+      messageBox.setAttribute("data-listener-attached", "true");
+
+      messageBox.addEventListener("focusin", () =>
+        handleFocus(ctx, messageBox)
+      );
+      // messageBox.addEventListener("focusout", () => handleBlur());
+    }
+  });
+}
+async function handleFocus(ctx: ContentScriptContext, messageBox: Element) {
+  if (activeMessageBox !== messageBox) {
+    if (ui) {
+      ui.remove(); // button removal from the previously focused box
+      ui = null;
+    }
+    activeMessageBox = messageBox; // focusd box is active
+
+    // creating ui for the focused message box
+    ui = await createUi(ctx, messageBox);
+    ui.mount();
+  }
+}
+
+function handleBlur() {
+  if (ui) {
+    ui.remove();
+    ui = null;
+  }
+}
+
+// create ui fucution using shadowroot
+function createUi(ctx: ContentScriptContext, messageBox: Element) {
   const appContainer = document.createElement("div");
   const root: Root = createRoot(appContainer);
 
